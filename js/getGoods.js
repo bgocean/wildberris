a89e6726ef1a949d7264d33edfd198bbd109f67b
bgocean <bigocean700@gmail.com>
2023-11-30 23:17:44 +0200

    cart of goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
+  const more = document.querySelector(".more");
 
   const renderGoods = (goods) => {
     const goodsContainer = document.querySelector(".long-goods-list");
@@ -24,7 +25,7 @@ const getGoods = () => {
 					</div>
       `;
 
-      goodsContainer.append(goodBlock);      
+      goodsContainer.append(goodBlock);
     });
   };
 
@@ -60,6 +61,14 @@ const getGoods = () => {
     window.location.pathname === "/goods.html"
   ) {
     renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
+
+  if (more) {
+    more.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      getData();
+    });
   }
 };
 
